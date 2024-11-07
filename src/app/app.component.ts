7dd7614ca63474137d6dcd61cdc8b721ad7b239d
Nergy101 <cdijk4@gmail.com>
2024-11-07 16:49:15 +0100

    formatting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,184 +1,196 @@
-import { CommonModule } from '@angular/common';
-import { Component, inject, OnDestroy, OnInit, signal } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { AuthMethodsList, AuthModel, AuthProviderInfo } from 'pocketbase';
-import { PocketbaseService } from './pocketbase.service';
-import { ToastrService } from 'ngx-toastr';
+import { CommonModule } from "@angular/common";
+import { Component, inject, OnDestroy, OnInit, signal } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { AuthMethodsList, AuthModel, AuthProviderInfo } from "pocketbase";
+import { PocketbaseService } from "./pocketbase.service";
+import { ToastrService } from "ngx-toastr";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, FormsModule],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+    selector: "app-root",
+    standalone: true,
+    imports: [CommonModule, FormsModule],
+    templateUrl: "./app.component.html",
+    styleUrls: ["./app.component.scss"],
 })
 export class AppComponent implements OnInit, OnDestroy {
-  userRecords = signal<any[]>([]);
-  votesToVote = signal<any[]>([]);
+    userRecords = signal<any[]>([]);
+    votesToVote = signal<any[]>([]);
 
-  authMethods?: AuthMethodsList;
+    authMethods?: AuthMethodsList;
 
-  loggedInUser = signal<AuthModel | undefined>(undefined);
+    loggedInUser = signal<AuthModel | undefined>(undefined);
 
-  username = '';
-  password = '';
+    username = "";
+    password = "";
 
-  toastr = inject(ToastrService);
-  pbService = inject(PocketbaseService);
+    toastr = inject(ToastrService);
+    pbService = inject(PocketbaseService);
 
-  // Example: Subscribe to changes in the users and votes collections
-  constructor() {
-
-
-    const user = localStorage.getItem('auth');
-    if (user) {
-      this.loggedInUser.set(JSON.parse(user));
-    }
-
-    // Subscribe to changes in any users record
-    this.pbService.getCollection('users').subscribe(
-      '*',
-      (e) => {
-        // add record
-        if (e.action === 'create') {
-          console.log('User created:', e);
-          this.userRecords.update(currentValue => [...currentValue, e.record]);
+    // Example: Subscribe to changes in the users and votes collections
+    constructor() {
+        const user = localStorage.getItem("auth");
+        if (user) {
+            this.loggedInUser.set(JSON.parse(user));
         }
-      },
-      {
-        /* other options like expand, custom headers, etc. */
-      }
-    );
 
-    this.pbService.getCollection('polls').subscribe(
-      '*',
-      async (e) => {
-        // add record
-        if (e.action === 'update') {
-          console.log('Vote updated:', e);
-          const vote = this.votesToVote().find(vote => vote.id === e.record.id);
-          const index = this.votesToVote().indexOf(vote!);
+        // Subscribe to changes in any users record
+        this.pbService.getCollection("users").subscribe(
+            "*",
+            (e) => {
+                // add record
+                if (e.action === "create") {
+                    console.log("User created:", e);
+                    this.userRecords.update((currentValue) => [
+                        ...currentValue,
+                        e.record,
+                    ]);
+                }
+            },
+            {
+                /* other options like expand, custom headers, etc. */
+            },
+        );
 
-          const currentVotes = this.votesToVote();
+        this.pbService.getCollection("polls").subscribe("*", async (e) => {
+            // add record
+            if (e.action === "update") {
+                console.log("Vote updated:", e);
+                const vote = this.votesToVote().find(
+                    (vote) => vote.id === e.record.id,
+                );
+                const index = this.votesToVote().indexOf(vote!);
 
-          e.record.expand = {}
-          e.record.expand['created_by'] = await this.pbService.getRecord('users', e.record['created_by']);
+                const currentVotes = this.votesToVote();
 
-          currentVotes[index] = e.record;
-          this.votesToVote.set(currentVotes);
+                e.record.expand = {};
+                e.record.expand["created_by"] = await this.pbService.getRecord(
+                    "users",
+                    e.record["created_by"],
+                );
+
+                currentVotes[index] = e.record;
+                this.votesToVote.set(currentVotes);
+            }
+        });
+    }
+
+    // Example: setting records
+    async ngOnInit(): Promise<void> {
+        await this.setRecords();
+        await this.setAuthMethods();
+        await this.setVotesToVote();
+    }
+
+    async login(provider: AuthProviderInfo) {
+        await this.pbService
+            .getCollection("users")
+            .authWithOAuth2({ provider: provider.name });
+
+        console.log("Auth:", this.pbService.getAuthStore().token);
+
+        localStorage.setItem(
+            "auth",
+            JSON.stringify(this.pbService.getAuthStore().model),
+        );
+
+        this.loggedInUser.set(this.pbService.getAuthStore().model);
+        this.toastr.success("User logged in successfully");
+    }
+
+    async signupOrLogin() {
+        if (this.username === "" || this.password === "") {
+            this.toastr.error("Username and password are required");
+            return;
         }
-      }
-    );
-  }
 
-  // Example: setting records
-  async ngOnInit(): Promise<void> {
-    await this.setRecords();
-    await this.setAuthMethods();
-    await this.setVotesToVote();
-  }
+        try {
+            const newUser = await this.pbService.getCollection("users").create({
+                username: this.username,
+                password: this.password,
+                passwordConfirm: this.password, // Required to confirm password during creation
+            });
 
+            console.log("User created:", newUser);
+            this.toastr.success("User created successfully");
+        } catch (e) {
+            console.log("User already existed, logging in");
+            this.toastr.info("User already existed, logging in");
+        }
 
-  async login(provider: AuthProviderInfo) {
-    await this.pbService
-      .getCollection('users')
-      .authWithOAuth2({ provider: provider.name });
+        // Log the user in
+        const authData = await this.pbService
+            .getCollection("users")
+            .authWithPassword(this.username, this.password);
 
-    console.log('Auth:', this.pbService.getAuthStore().token);
+        console.log("User logged in:", authData);
+        this.toastr.success("User logged in successfully");
 
-    localStorage.setItem('auth', JSON.stringify(this.pbService.getAuthStore().model));
-
-    this.loggedInUser.set(this.pbService.getAuthStore().model);
-    this.toastr.success('User logged in successfully');
-  }
-
-  async signupOrLogin() {
-
-    if (this.username === '' || this.password === '') {
-
-      this.toastr.error('Username and password are required');
-      return
+        this.loggedInUser.set(authData.record);
+        await this.setRecords();
     }
 
-    try {
-      const newUser = await this.pbService.getCollection('users').create({
-        username: this.username,
-        password: this.password,
-        passwordConfirm: this.password, // Required to confirm password during creation
-      });
-
-      console.log('User created:', newUser);
-      this.toastr.success('User created successfully');
-    } catch (e) {
-
-      console.log('User already existed, logging in');
-      this.toastr.info('User already existed, logging in');
+    logout() {
+        this.pbService.getAuthStore().clear();
+        this.loggedInUser.set(undefined);
+        localStorage.removeItem("auth");
+        console.log("User logged out");
+        this.toastr.info("User logged out");
     }
 
-    // Log the user in
-    const authData = await this.pbService
-      .getCollection('users')
-      .authWithPassword(this.username, this.password);
+    voteForOptionA(voteId: string) {
+        const vote = this.votesToVote().find((vote) => vote.id === voteId);
 
-    console.log('User logged in:', authData);
-    this.toastr.success('User logged in successfully');
+        if (vote.users_who_voted.includes(this.loggedInUser()!["id"])) {
+            this.toastr.error("You have already voted for this poll");
+            return;
+        }
 
-    this.loggedInUser.set(authData.record);
-    await this.setRecords();
-  }
-
-  logout() {
-    this.pbService.getAuthStore().clear();
-    this.loggedInUser.set(undefined);
-    localStorage.removeItem('auth');
-    console.log('User logged out');
-    this.toastr.info('User logged out');
-  }
-
-  voteForOptionA(voteId: string) {
-    const vote = this.votesToVote().find(vote => vote.id === voteId);
-
-    if (vote.users_who_voted.includes(this.loggedInUser()!['id'])) {
-      this.toastr.error('You have already voted for this poll');
-      return;
+        this.pbService.updateRecord("polls", vote!.id, {
+            option_a: vote!.option_a + 1,
+            users_who_voted: [
+                ...vote!.users_who_voted,
+                this.loggedInUser()!["id"],
+            ],
+        });
     }
 
-    this.pbService.updateRecord('polls', vote!.id, {
-      option_a: vote!.option_a + 1,
-      users_who_voted: [...vote!.users_who_voted, this.loggedInUser()!['id']]
-    })
-  }
+    voteForOptionB(voteId: string) {
+        const vote = this.votesToVote().find((vote) => vote.id === voteId);
 
-  voteForOptionB(voteId: string) {
-    const vote = this.votesToVote().find(vote => vote.id === voteId);
+        if (vote.users_who_voted.includes(this.loggedInUser()!["id"])) {
+            this.toastr.error("You have already voted for this poll");
+            return;
+        }
 
-    if (vote.users_who_voted.includes(this.loggedInUser()!['id'])) {
-      this.toastr.error('You have already voted for this poll');
-      return;
+        this.pbService.updateRecord("polls", vote!.id, {
+            option_b: vote!.option_b + 1,
+            users_who_voted: [
+                ...vote!.users_who_voted,
+                this.loggedInUser()!["id"],
+            ],
+        });
     }
 
-    this.pbService.updateRecord('polls', vote!.id, {
-      option_b: vote!.option_b + 1,
-      users_who_voted: [...vote!.users_who_voted, this.loggedInUser()!['id']]
-    })
-  }
+    private async setRecords() {
+        this.userRecords.set(
+            (await this.pbService.getRecords("users")) as any[],
+        );
+    }
 
-  private async setRecords() {
-    this.userRecords.set((await this.pbService.getRecords('users')) as any[]);
-  }
+    private async setAuthMethods() {
+        this.authMethods = await this.pbService.getAuthMethods();
+    }
 
-  private async setAuthMethods() {
-    this.authMethods = await this.pbService.getAuthMethods();
-  }
+    private async setVotesToVote() {
+        this.votesToVote.set(
+            (await this.pbService.getRecords("polls", {
+                expand: "created_by",
+            })) as any[],
+        );
+    }
 
-  private async setVotesToVote() {
-    this.votesToVote.set((await this.pbService.getRecords('polls',
-      { expand: 'created_by' }
-    )) as any[]);
-  }
-
-  ngOnDestroy(): void {
-    this.pbService.getCollection('users').unsubscribe('*'); // Unsubscribe from all events
-    this.pbService.getCollection('polls').unsubscribe('*'); // Unsubscribe from all events
-  }
+    ngOnDestroy(): void {
+        this.pbService.getCollection("users").unsubscribe("*"); // Unsubscribe from all events
+        this.pbService.getCollection("polls").unsubscribe("*"); // Unsubscribe from all events
+    }
 }
--- src/app/pocketbase.service.ts
+++ src/app/pocketbase.service.ts
@@ -1,73 +1,73 @@
-import { Injectable } from '@angular/core';
-import PocketBase, { AuthMethodsList } from 'pocketbase';
+import { Injectable } from "@angular/core";
+import PocketBase, { AuthMethodsList } from "pocketbase";
 
 @Injectable({
-  providedIn: 'root',
+    providedIn: "root",
 })
 export class PocketbaseService {
-  private pb: PocketBase;
+    private pb: PocketBase;
 
-  constructor() {
-    // Initialize the PocketBase client
-    this.pb = new PocketBase('https://cloud-republic-demo.pockethost.io/');
-  }
+    constructor() {
+        // Initialize the PocketBase client
+        this.pb = new PocketBase("https://cloud-republic-demo.pockethost.io/");
+    }
 
-  // Example: Get all auth methods
-  async getAuthMethods(): Promise<AuthMethodsList> {
-    const result = await this.pb.collection('users').listAuthMethods();
-    return result;
-  }
+    // Example: Get all auth methods
+    async getAuthMethods(): Promise<AuthMethodsList> {
+        const result = await this.pb.collection("users").listAuthMethods();
+        return result;
+    }
 
-  // Example: Get records from a collection
-  getRecords(collection: string, options?: any) {
-    return this.pb.collection(collection).getFullList({
-      ...options,
-    });
-  }
+    // Example: Get records from a collection
+    getRecords(collection: string, options?: any) {
+        return this.pb.collection(collection).getFullList({
+            ...options,
+        });
+    }
 
-  // Example: Get a single record from
-  getRecord(collection: string, id: string, options?: any) {
-    return this.pb.collection(collection).getOne(id, {
-      ...options,
-    });
-  }
+    // Example: Get a single record from
+    getRecord(collection: string, id: string, options?: any) {
+        return this.pb.collection(collection).getOne(id, {
+            ...options,
+        });
+    }
 
-  // Example: Get a collection
-  getCollection(collection: string) {
-    return this.pb.collection(collection);
-  }
+    // Example: Get a collection
+    getCollection(collection: string) {
+        return this.pb.collection(collection);
+    }
 
-  // Example: Get the auth store
-  getAuthStore() {
-    return this.pb.authStore;
-  }
+    // Example: Get the auth store
+    getAuthStore() {
+        return this.pb.authStore;
+    }
 
-  // Example: Create a new record in a collection
-  createRecord(collection: string, data: any) {
-    return this.pb.collection(collection).create(data);
-  }
+    // Example: Create a new record in a collection
+    createRecord(collection: string, data: any) {
+        return this.pb.collection(collection).create(data);
+    }
 
-  updateRecord(collection: string, id: string, data: any) {
-    return this.pb.collection(collection).update(id, data);
-  }
+    updateRecord(collection: string, id: string, data: any) {
+        return this.pb.collection(collection).update(id, data);
+    }
 
-  // Example: Delete a record from a collection
-  deleteRecord(collection: string, id: string) {
-    return this.pb.collection(collection).delete(id);
-  }
+    // Example: Delete a record from a collection
+    deleteRecord(collection: string, id: string) {
+        return this.pb.collection(collection).delete(id);
+    }
 
-  // Example: Authenticate user
-  login(email: string, password: string) {
-    return this.pb.collection('users').authWithPassword(email, password);
-  }
+    // Example: Authenticate user
+    login(email: string, password: string) {
+        return this.pb.collection("users").authWithPassword(email, password);
+    }
 
-  // Example: Authenticate admin
-  loginAdmin(email: string, password: string) {
-    return this.pb.collection('admins').authWithPassword(email, password);
-  }
+    // Example: Authenticate admin
+    loginAdmin(email: string, password: string) {
+        return this.pb.collection("admins").authWithPassword(email, password);
+    }
 
-  // Example: Log out user
-  logout() {
-    this.pb.authStore.clear();
-  }
+    // Example: Log out user
+    logout() {
+        this.pb.authStore.clear();
+    }
 }
